3f9d20dd6e28cc5ac80fe80ede503c10449ace6d
Namoj-design <namojperiakumar@gmail.com>
2025-12-28 19:47:17 +0530

    hedera - accountcraetion

---
+++ apps/custody-backend/src/hedera/account.ts
@@ -0,0 +1,29 @@
+import {
+    AccountCreateTransaction,
+    PublicKey,
+    Hbar,
+  } from "@hashgraph/sdk";
+  import { createHederaClient } from "./client";
+  
+  /**
+   * Creates a Hedera account controlled by an MPC public key
+   */
+  export async function createMpcAccount(
+    mpcPublicKeyBytes: Uint8Array
+  ) {
+    const client = createHederaClient();
+  
+    const publicKey = PublicKey.fromBytes(mpcPublicKeyBytes);
+  
+    const tx = await new AccountCreateTransaction()
+      .setKey(publicKey)
+      .setInitialBalance(new Hbar(5)) // testnet balance
+      .execute(client);
+  
+    const receipt = await tx.getReceipt(client);
+  
+    return {
+      accountId: receipt.accountId?.toString(),
+      status: receipt.status.toString(),
+    };
+  }
